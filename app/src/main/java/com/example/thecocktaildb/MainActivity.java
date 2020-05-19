dd568bbd33ec11b1a98da9b20207fb7114f2da0b
Nazar Chumbey <nazzichumbey@gmail.com>
2020-05-19 12:30:04 +0300

    Added search toolbar and text "History is empty"

--- app/src/main/java/com/example/thecocktaildb/MainActivity.java
+++ app/src/main/java/com/example/thecocktaildb/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.thecocktaildb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MaterialSearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Material Search");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_item,menu);
+            MenuItem item  = menu.findItem(R.id.action_search);
+            searchView.setMenuItem(item);
+
+        return true;
     }
 }
